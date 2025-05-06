8d1b8acbf4db3f3fe75f7205536c7cd2087584a5
Joy <joy.liechti@bbz-cfp.ch>
2025-05-06 13:00:23 +0000

    bleep

--- index.js
+++ index.js
@@ -13,12 +13,40 @@ app.get("/", async function (req, res) {
   res.render("start", {});
 });
 
-app.get("/impressum", async function (req, res) {
-  res.render("impressum", {});
+app.get("/subject", async function (req, res) {
+  res.render("subject", {});
 });
 
-app.get("/recipe", async function (req, res) {
-  res.render("posts", {});
+app.get("/subject2", async function (req, res) {
+  res.render("subject2", {});
+});
+
+app.get("/subject3", async function (req, res) {
+  res.render("subject3", {});
+});
+
+app.get("/subject4", async function (req, res) {
+  res.render("subject4", {});
+});
+
+app.get("/themes", async function (req, res) {
+  res.render("themes", {});
+});
+
+app.get("/themes2", async function (req, res) {
+  res.render("themes2", {});
+});
+
+app.get("/themes3", async function (req, res) {
+  res.render("themes3", {});
+});
+
+app.get("/themes4", async function (req, res) {
+  res.render("themes4", {});
+});
+
+app.get("/themes5", async function (req, res) {
+  res.render("themes5", {});
 });
 
 /* Wichtig! Diese Zeilen müssen immer am Schluss der Website stehen! */
